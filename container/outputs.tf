692f0785f727ccc269094410762e134ef493385f
Pedro Almeida <pedro@MBP-de-Pedro.Home>
2023-10-27 14:26:40 +0100

    Improve outputs

--- container/outputs.tf
+++ container/outputs.tf
@@ -1,15 +1,4 @@
-# output "container-name" {
-#   value       = docker_container.nodered_container.name
-#   description = "The name of the container"
-# }
 
-# output "container-ip" {
-#   value       = [for i in docker_container.nodered_container[*] : join(":", [i.network_data[0]["ip_address"]], i.ports[*]["external"])]
-#   description = "The IP Address and external port of the container"
-# }
-
-
-# output "host-testing-link" {
-#   value       = join(":", ["http", "//localhost", docker_container.nodered_container[0].ports[0].external])
-#   description = "The link to perform test from the host machine"
-# }
+output "application_access" {
+    value = {for x in docker_container.app_container[*]: x.name => join(":", [x.network_data[0].ip_address], x.ports[*]["external"])}
+}
--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,12 @@
 
 
 # output "host-testing-link" {
-#   value       = join(":", ["http", "//localhost", docker_container.nodered_container[0].ports[0].external])
+#   value       = join(":", ["http", "//localhost", docker_container.app_container[0].ports[0].external])
 #   description = "The link to perform test from the host machine"
 # }
+
+
+output "application_access" {
+    value = [for x in module.container[*]: x]
+    description = "The name and socket for each application"
+}
